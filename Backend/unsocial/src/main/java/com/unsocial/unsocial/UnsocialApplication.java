621d06945b4be634bff3d08a20bc4f6a9ee77133
Khushabu Jain <jainkhushbu1780@gmail.com>
2026-06-02 16:45:38 +0530

    Initialize Spring Boot backend project

---
+++ Backend/unsocial/src/main/java/com/unsocial/unsocial/UnsocialApplication.java
@@ -0,0 +1,13 @@
+package com.unsocial.unsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsocialApplication.class, args);
+	}
+
+}
---
+++ Backend/unsocial/src/test/java/com/unsocial/unsocial/UnsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsocial.unsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
